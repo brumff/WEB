988ccef39b32b49b1079d7ba6d077aa2593d5150
brumff <brunameiraf@gmail.com>
2022-11-07 20:25:49 -0300

    Alteração

--- demo/src/main/java/br/com/pethoteis/demo/controle/PetControle.java
+++ demo/src/main/java/br/com/pethoteis/demo/controle/PetControle.java
@@ -3,6 +3,7 @@ package br.com.pethoteis.demo.controle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,14 +20,29 @@ public class PetControle {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("html/cadastrodopet");
         mv.addObject("Pet", new Pet());
+        mv.addObject("petList", petrepositorio.findAll());
         return mv;
     }
     @PostMapping("InsertPet")
     public ModelAndView inserirPet(Pet pet){
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("redirect:/Pet/listPet");
+        mv.setViewName("redirect:/inserirPet");
         petrepositorio.save(pet);
         return mv;
+    }    
+    @GetMapping("/alterarpet/{codigo}")
+    public ModelAndView alterar(@PathVariable("codigo") Integer codigo){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("html/alterarpet");
+        Pet pet = petrepositorio.getOne(codigo);
+        mv.addObject("pet", pet);
+        return mv;
     }
-    
+    @PostMapping("/alterarpet")
+    public ModelAndView alterar(Pet pet){
+        ModelAndView mv = new ModelAndView();
+        petrepositorio.save(pet);
+        mv.setViewName("redirect:/inserirPet");
+        return mv;
+    }
 }
